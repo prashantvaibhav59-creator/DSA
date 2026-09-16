b67c960c21c9597c01ed95ce86ad726ce38e09c8
prashantvaibhav59-creator <prashantvaibhav59@gmail.com>
2026-09-16 21:54:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (17.71%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            long long square = 1LL * mid * mid;
+
+            if (square == num) {
+                return true;
+            }
+            else if (square < num) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
